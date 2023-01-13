247e8c850efd5d022f029bc218c7421af0804a7b
YUXUANHUANG-UCL <uceeuam@ucl.ac.uk>
2023-01-13 11:59:33 +0000

    Final edition

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-
 from A1.A1 import get_A1_results
 from A2.A2 import get_A2_results
 from B1.B1 import get_B1_results
 from B2.B2 import get_B2_results
 
-'''
 get_A1_results()
 get_A2_results()
 get_B1_results()
-'''
 get_B2_results()
